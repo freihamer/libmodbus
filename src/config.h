8571c2b87bc460ab16a47c5091cc5581d10fced3
Luca Barbiero <luca.barbiero@hypersonica.space>
2025-10-15 22:47:34 +0200

    attempting to unify config.h between win and linux

--- src/config.h
+++ src/config.h
@@ -1,164 +1,141 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
-
-/* Define to 1 if you have the <arpa/inet.h> header file. */
-/* #undef HAVE_ARPA_INET_H */
-
-/* Define to 1 if you have the declaration of `TIOCSRS485', and to 0 if you
-   don't. */
-/* #undef HAVE_DECL_TIOCSRS485 */
-
-/* Define to 1 if you have the declaration of `__CYGWIN__', and to 0 if you
-   don't. */
-/* #undef HAVE_DECL___CYGWIN__ */
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-/* #undef HAVE_DLFCN_H */
-
-/* Define to 1 if you have the <errno.h> header file. */
-#define HAVE_ERRNO_H 1
-
-/* Define to 1 if you have the <fcntl.h> header file. */
-#define HAVE_FCNTL_H 1
-
-/* Define to 1 if you have the `fork' function. */
-/* #undef HAVE_FORK */
-
-/* Define to 1 if you have the `getaddrinfo' function. */
-/* #undef HAVE_GETADDRINFO */
-
-/* Define to 1 if you have the `gettimeofday' function. */
-/* #undef HAVE_GETTIMEOFDAY */
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the <limits.h> header file. */
-#define HAVE_LIMITS_H 1
-
-/* Define to 1 if you have the <linux/serial.h> header file. */
-/* #undef HAVE_LINUX_SERIAL_H */
-
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the `memset' function. */
-#define HAVE_MEMSET 1
-
-/* Define to 1 if you have the <netdb.h> header file. */
-/* #undef HAVE_NETDB_H */
-
-/* Define to 1 if you have the <netinet/in.h> header file. */
-/* #undef HAVE_NETINET_IN_H */
-
-/* Define to 1 if you have the <netinet/tcp.h> header file. */
-/* #undef HAVE_NETINET_TCP_H */
-
-/* Define to 1 if you have the `select' function. */
-/* #undef HAVE_SELECT */
-
-/* Define to 1 if you have the `socket' function. */
-/* #undef HAVE_SOCKET */
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the `strerror' function. */
-#define HAVE_STRERROR 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-/* #undef HAVE_STRINGS_H */
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if you have the `strlcpy' function. */
-/* #undef HAVE_STRLCPY */
-
-/* Define to 1 if you have the <sys/ioctl.h> header file. */
-/* #undef HAVE_SYS_IOCTL_H */
-
-/* Define to 1 if you have the <sys/socket.h> header file. */
-/* #undef HAVE_SYS_SOCKET_H */
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/time.h> header file. */
-/* #undef HAVE_SYS_TIME_H */
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <termios.h> header file. */
-/* #undef HAVE_TERMIOS_H */
-
-/* Define to 1 if you have the <time.h> header file. */
-#define HAVE_TIME_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-/* #undef HAVE_UNISTD_H */
-
-/* Define to 1 if you have the `vfork' function. */
-/* #undef HAVE_VFORK */
-
-/* Define to 1 if you have the <vfork.h> header file. */
-/* #undef HAVE_VFORK_H */
-
-/* Define to 1 if you have the <winsock2.h> header file. */
-#define HAVE_WINSOCK2_H 1
-
-/* Define to 1 if `fork' works. */
-/* #undef HAVE_WORKING_FORK */
-
-/* Define to 1 if `vfork' works. */
-/* #undef HAVE_WORKING_VFORK */
-
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
+/*
+ * Unified config.h for libmodbus (cross-platform)
+ * Derived from autotools-generated configs for Linux and Windows
+ *
+ * This file replaces the generated config.h and is maintained manually
+ * in the CMake-based build system.
  */
-/* #undef LT_OBJDIR */
 
-/* Name of package */
-#define PACKAGE "libmodbus"
+#pragma once
 
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://github.com/stephane/libmodbus/issues"
+/* ============================================================
+ *  PLATFORM DETECTION
+ * ============================================================ */
+#if defined(_WIN32) || defined(_WIN64)
+#  define LIBMODBUS_PLATFORM_WINDOWS 1
+#elif defined(__linux__)
+#  define LIBMODBUS_PLATFORM_LINUX 1
+#elif defined(__APPLE__)
+#  define LIBMODBUS_PLATFORM_MACOS 1
+#else
+#  define LIBMODBUS_PLATFORM_UNKNOWN 1
+#endif
 
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "libmodbus"
+/* ============================================================
+ *  COMMON DEFINES
+ * ============================================================ */
+#define PACKAGE_NAME        "libmodbus"
+#define PACKAGE_VERSION     "3.1.11"
+#define PACKAGE_STRING      PACKAGE_NAME " " PACKAGE_VERSION
+#define PACKAGE_TARNAME     "libmodbus"
+#define PACKAGE_BUGREPORT   "https://github.com/freihamer/libmodbus/issues"
+#define PACKAGE_URL         "https://libmodbus.org/"
+#define VERSION             PACKAGE_VERSION
+#define STDC_HEADERS        1
 
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libmodbus 3.1.11"
+/* ============================================================
+ *  LINUX / UNIX CONFIGURATION
+ * ============================================================ */
+#if LIBMODBUS_PLATFORM_LINUX || LIBMODBUS_PLATFORM_MACOS
 
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libmodbus"
+#  define HAVE_UNISTD_H          1
+#  define HAVE_SYS_TYPES_H       1
+#  define HAVE_SYS_STAT_H        1
+#  define HAVE_SYS_SOCKET_H      1
+#  define HAVE_SYS_IOCTL_H       1
+#  define HAVE_SYS_TIME_H        1
+#  define HAVE_ARPA_INET_H       1
+#  define HAVE_NETDB_H           1
+#  define HAVE_NETINET_IN_H      1
+#  define HAVE_NETINET_TCP_H     1
+#  define HAVE_NETINET_IP_H      1
+#  define HAVE_FCNTL_H           1
+#  define HAVE_ERRNO_H           1
+#  define HAVE_TIME_H            1
+#  define HAVE_LIMITS_H          1
+#  define HAVE_STRING_H          1
+#  define HAVE_STRINGS_H         1
+#  define HAVE_STDINT_H          1
+#  define HAVE_STDLIB_H          1
+#  define HAVE_STDIO_H           1
+#  define HAVE_TERMIOS_H         1
+#  define HAVE_SELECT            1
+#  define HAVE_SOCKET            1
+#  define HAVE_GAI_STRERROR      1
+#  define HAVE_GETADDRINFO       1
+#  define HAVE_GETTIMEOFDAY      1
+#  define HAVE_INET_NTOP         1
+#  define HAVE_INET_PTON         1
+#  define HAVE_DECL_TIOCM_RTS    1
+#  define HAVE_DECL_TIOCSRS485   1
 
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
+/* Extensions normally enabled by autotools */
+#  ifndef _GNU_SOURCE
+#    define _GNU_SOURCE 1
+#  endif
+#  ifndef _DEFAULT_SOURCE
+#    define _DEFAULT_SOURCE 1
+#  endif
+#  ifndef _POSIX_C_SOURCE
+#    define _POSIX_C_SOURCE 200112L
+#  endif
 
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "3.1.11"
+#endif /* UNIX */
 
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
 
-/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-/* #undef TIME_WITH_SYS_TIME */
+/* ============================================================
+ *  WINDOWS CONFIGURATION
+ * ============================================================ */
+#if LIBMODBUS_PLATFORM_WINDOWS
 
-/* Version number of package */
-#define VERSION "3.1.11"
+#  define HAVE_ERRNO_H       1
+#  define HAVE_FCNTL_H       1
+#  define HAVE_INTTYPES_H    1
+#  define HAVE_LIMITS_H      1
+#  define HAVE_MEMORY_H      1
+#  define HAVE_STDINT_H      1
+#  define HAVE_STDLIB_H      1
+#  define HAVE_STRING_H      1
+#  define HAVE_SYS_TYPES_H   1
+#  define HAVE_SYS_STAT_H    1
+#  define HAVE_TIME_H        1
+#  define HAVE_WINSOCK2_H    1
+#  define STDC_HEADERS       1
+#  define WINVER             0x0501  /* Windows XP and later */
+#  define _WIN32_WINNT       0x0501
 
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
+/* No fork/vfork/select/socket on Windows POSIX */
+#  undef  HAVE_UNISTD_H
+#  undef  HAVE_SYS_SOCKET_H
+#  undef  HAVE_SYS_IOCTL_H
+#  undef  HAVE_NETDB_H
+#  undef  HAVE_NETINET_TCP_H
+#  undef  HAVE_NETINET_IN_H
+#  undef  HAVE_ARPA_INET_H
+#  undef  HAVE_SELECT
+#  undef  HAVE_SOCKET
 
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef pid_t */
+#endif /* WINDOWS */
 
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
+/* ============================================================
+ *  COMPILER / EXTENSION FLAGS
+ * ============================================================ */
 
-/* Define as `fork' if `vfork' does not work. */
-#define vfork fork
+/* Enable general POSIX/GNU extensions (needed for IPTOS_LOWDELAY, etc.) */
+#ifndef _GNU_SOURCE
+#  define _GNU_SOURCE 1
+#endif
+
+#ifndef _DEFAULT_SOURCE
+#  define _DEFAULT_SOURCE 1
+#endif
+
+/* Solaris / HP-UX legacy support (no effect on Linux/Windows) */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+#  define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+
+/* For static linking */
+#define LT_OBJDIR ".libs/"
+
+
